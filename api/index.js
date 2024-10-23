75414121680484714717b96503c1d52333380822
Prathamesh Lakare <prathameshlakare001@gmail.com>
2024-10-23 15:10:17 +0530

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -320,7 +320,7 @@ app.delete("/api/user/address/:id", async (req, res) => {
   try {
     const deletedAddress = await Address.findByIdAndDelete(req.params.id);
     if (deletedAddress) {
-      res.status(200).json({ message: "Address deleted successfully" });
+      res.status(200).json(deletedAddress);
     } else {
       res.status(404).json({ error: "Address not found." });
     }
